76f701e506d210c200d92959aab8dba074109d11
leeseungjun <cjstkrak@gmail.com>
2024-02-16 16:23:49 +0900

    fix: middleware를 access토큰존재로만 검증

--- src/auth/middlewares/tokenValidation.middleware.ts
+++ src/auth/middlewares/tokenValidation.middleware.ts
@@ -8,33 +8,15 @@ import { error } from 'console';
 
 @Injectable()
 export class TokenValidationMiddleware implements NestMiddleware {
-  constructor(
-    private readonly jwtService: JwtService,
-    private readonly configService: ConfigService,
-    private readonly tokenBlacklistService: TokenBlacklistService,
-  ) {}
+  constructor() // private readonly jwtService: JwtService,
+  // private readonly configService: ConfigService,
+  // private readonly tokenBlacklistService: TokenBlacklistService,
+  {}
   async use(request: Request, res: Response, next: NextFunction) {
     try {
       const accessToken = request.body.accessToken;
       const refreshToken = request.body.refreshToken;
-      const [decordingAccess, decordingRefresh] = await Promise.all([
-        // 비동기 함수의 병렬처리를 위해서
-        this.jwtService.verifyAsync(accessToken, {
-          secret: this.configService.get<string>('JWT_SECRET'),
-        }),
-        this.jwtService.verifyAsync(refreshToken, {
-          secret: this.configService.get<string>('JWT_SECRET'),
-        }),
-      ]);
-      const [accessTokenInBlacklist, refreshTokenInBlacklist] =
-        await Promise.all([
-          this.tokenBlacklistService.isTokenBlacklisted(decordingAccess.jti),
-          this.tokenBlacklistService.isTokenBlacklisted(decordingRefresh.jti),
-        ]);
-
-      if (accessTokenInBlacklist || refreshTokenInBlacklist) {
-        throw new error();
-      }
+      if (!accessToken || !refreshToken) throw error;
     } catch (error) {
       throw new BusinessException(
         'invalid-token',
@@ -47,3 +29,33 @@ export class TokenValidationMiddleware implements NestMiddleware {
     next();
   }
 }
+
+// try{
+// const accessToken = request.body.accessToken;
+// const refreshToken = request.body.refreshToken;
+// const [decordingAccess, decordingRefresh] = await Promise.all([
+//   // 비동기 함수의 병렬처리를 위해서
+//   this.jwtService.verifyAsync(accessToken, {
+//     secret: this.configService.get<string>('JWT_SECRET'),
+//   }),
+//   this.jwtService.verifyAsync(refreshToken, {
+//     secret: this.configService.get<string>('JWT_SECRET'),
+//   }),
+// ]);
+// const [accessTokenInBlacklist, refreshTokenInBlacklist] =
+//   await Promise.all([
+//     this.tokenBlacklistService.isTokenBlacklisted(decordingAccess.jti),
+//     this.tokenBlacklistService.isTokenBlacklisted(decordingRefresh.jti),
+//   ]);
+
+// if (accessTokenInBlacklist || refreshTokenInBlacklist) {
+//   throw new error();
+// }
+// } catch (error) {
+// throw new BusinessException(
+//   'invalid-token',
+//   'fail-token-validation',
+//   'fail token validation',
+//   HttpStatus.UNAUTHORIZED,
+// );
+// }
